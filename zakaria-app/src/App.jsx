b1e06f76c4f59c65e6cb74198b128739631494b0
Erfan Shahani <shahani.bjj2008@gmail.com>
2025-11-28 11:23:51 +0330

    feat: add new files

---
+++ zakaria-app/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+import Project from './Project.jsx';
+
+
+function App(){
+    return(
+        <>
+    <Project/>
+        </>
+    )
+}
+export default App;
---
+++ zakaria-app/src/Project.jsx
@@ -0,0 +1,86 @@
+import './Project.css';
+
+function Project(){
+    return(
+        <>
+     <div class="card-container">
+
+<div class="id-card">
+
+    <div class="card-header">
+
+        <span class="logo">شرکت فناوری زکریا</span>
+
+    </div>
+
+
+
+    <div class="card-body">
+
+        <div class="profile-photo">
+
+            <img src="https://via.placeholder.com/150/61afef/fff?text=Photo" alt="عکس پرسنلی"/>
+
+        </div>
+
+       
+
+        <h2 class="name"> عرفان شاهانی</h2>
+
+        <p class="title">توسعه‌دهنده ارشد طراحی وب</p>
+
+    </div>
+
+
+
+    <div class="card-footer">
+
+        <div class="info-row">
+
+            <i class="fas fa-id-card"></i>
+
+            <span>کد پرسنلی: </span>
+
+            <span class="info-value">104529</span>
+
+        </div>
+
+        <div class="info-row">
+
+            <i class="fas fa-envelope"></i>
+
+            <span>ایمیل: </span>
+
+            <span class="info-value"><a href="mailto:shahani.bjj2008@gmail.com" class="email">shahani.bjj2008@gmail.com</a></span>
+
+        </div>
+
+        <div class="info-row">
+
+            <i class="fas fa-phone"></i>
+
+            <span>داخلی: </span>
+
+            <span class="info-value">401</span>
+
+        </div>
+<div class="zakaria">
+        <a href="https://www.zakariait.ir" class="zakaria1" target="_blank">zakariait.ir</a>
+
+</div>
+
+    </div>
+
+   
+
+    <div class="design-element"></div>
+
+</div>
+
+
+
+</div>
+        </>
+    )
+}
+export default Project;
